8d608fb37bc447abcef388b9292019fda9b5fc7b
beat-kuliah <if-18056@students.ithb.ac.id>
2022-07-13 11:36:03 +0700

    add header

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,66 +1,72 @@
+<nav class="navbar navbar-expand-xl navbar-light" style="background-color: #6c6b86;">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ URL::to('images/Landing Page/logo cf ver 1-02.png') }}" alt="" width="60" height="60">
+        </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample06" aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    {{-- <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" class="nav-link px-2 link-dark">Home</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
-          </ul>
-    
-          <div class="col-md-3 text-end">
-            <button type="button" class="btn btn-outline-primary me-2">Login</button>
-            <button type="button" class="btn btn-primary">Sign-up</button>
-          </div>
-        </div>
-      </div> --}}
-    
-    
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav col-12 col-md-auto mb-2 justify-content-center">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
+      <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample06">
+        <ul class="navbar-nav ms-auto me-auto pe-4 mb-6 mb-xl-0">
+            <li class="nav-item">
+                <a class="nav-link mid-link" href="">CC</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link mid-link" href="">COD</a>
+            </li>
+            <li class="nav-item">
+                <a class="mid-link nav-link" href="">CIP</a>
+            </li>
+            <li class="nav-item">
+                <a class="mid-link nav-link" href="">Semnas</a>
+            </li>
+            <li class="nav-item">
+                <a class="mid-link nav-link" href="">Chempaign</a>
+            </li>
+            <li class="nav-item">
+                <a class="mid-link nav-link" href="">News</a>
+            </li>
         </ul>
+        
+            @guest
+            <ul class="navbar-nav ps-auto mb-xl-0">
+                @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="nav-link right-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                @endif
 
-        <div class="col-md-3 text-end">
-            <button type="button" class="btn btn-outline-primary me-2">Login</button>
-            <button type="button" class="btn btn-primary">Sign-up</button>
-          </div>
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link right-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+                </ul>
+            @else
+            <ul class="navbar-nav pe-2 ps-auto mb-xl-0">
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle right-link" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            </ul>
+            @endguest
       </div>
     </div>
   </nav>
-
-
-
 
 
 {{-- <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: #6c6b86;">
