27461565359df2cc8ae315c818f6d6ac3edb1793
beat-kuliah <if-18056@students.ithb.ac.id>
2022-07-28 08:57:12 +0700

    chemistry competition

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -11,11 +11,11 @@ class DownloadController extends Controller
 
     public function index($id)
     {
-        $fileName = 'buku_panduan\\' . $id . '.pdf';
+        $fileName = 'public\\buku_panduan\\' . $id . '.pdf';
         $headers = ['Content-Type: application/pdf'];
         $path = public_path($fileName);
 
-        $result = Response()->download($path);
+        $result = Response()->download($fileName);
 
         return $result;
     }
